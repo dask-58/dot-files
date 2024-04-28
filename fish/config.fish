1e06172c28b85815d405e77fdaf5726a38285e1b
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-04-28 15:10:58 +0530

    Remove: fish and copilot

--- fish/config.fish
+++
@@ -1,8 +0,0 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/dhruvkoli/anaconda3/bin/conda
-    eval /Users/dhruvkoli/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
